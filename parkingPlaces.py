7e2372e912bda0515760b089d3cc86ec98811560
Ni4ka <astahova_nica@mail.ru>
2021-04-13 11:18:29 +0300

    Do a simple version parkingPlaces.py

--- parkingPlaces.py
+++ parkingPlaces.py
@@ -11,23 +11,15 @@ FREE_PLACE = 3
 
 system( "clear" )
 
+
 print( "#" * PARKING_PLACES * 5 )
+ 
 
 for place_index in range( 1, PARKING_PLACES + 1 ):
-
- for i in range( 1, FREE_PLACE + 1):
-  
-  free_place = randint( 1, PARKING_PLACES )
-
-  if( place_index == free_place ):
-   print( "| 0 |", end = "" )
-   break  
-
-  else:
-   print( "| X |", end = "" )
-   break
-  
-
-
+ 
+ if( place_index == FREE_PLACE ):
+  print( "|   |", end = "" )
+ else:
+  print( "| X |", end = "" )
 
 print( "\n","#" * PARKING_PLACES * 5, sep = "" )
